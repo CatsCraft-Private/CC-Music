a97e1a6f81d6ec8f96752a12aa975e03627a7abe
CatsCraft-Private <cbbdevs@gmailcom>
2017-04-17 09:57:16 -0400

    Updated Code

--- src/bobcatsss/music/Main.java
+++ src/bobcatsss/music/Main.java
@@ -1,6 +1,6 @@
 package bobcatsss.music;
 
-import java.io.File;
+import java.io.*;
 import java.util.*;
 
 import com.xxmicloxx.NoteBlockAPI.*;
@@ -11,22 +11,35 @@ import org.bukkit.plugin.java.JavaPlugin;
 import simple.brainsynder.utils.ObjectPager;
 
 public class Main extends JavaPlugin {
-	public static Main plugin;
-	ObjectPager<Song> musicList;
-        Map<Player, SongPlayer> songPlayerMap = new HashMap<>();
+    public static Main plugin;
+    Map<Player, SongPlayer> songPlayerMap = new HashMap<>();
 
-	public void onEnable() {
-		plugin = this;
-		Bukkit.getPluginManager().registerEvents(new GUIClickEvent(), this);
-		getCommand("music").setExecutor(new MusicGUICommand());
-	}
+    public void onEnable() {
+        plugin = this;
+        Bukkit.getPluginManager().registerEvents(new GUIClickEvent(), this);
+        getCommand("music").setExecutor(new MusicGUICommand());}
 
-	public ObjectPager<Song> reloadSongs () {
+    public ObjectPager<Song> reloadSongs(int size) {
         File folder = new File(getDataFolder().toString() + "/songs/");
+        if (!folder.exists()) try {
+            folder.createNewFile();
+        } catch (IOException ignored) {}
+        File[] files = folder.listFiles();
+        if (files.length == 0) {
+            saveResource("/songs/Boom Clap.nbs", true);
+            saveResource("/songs/Cat_s In the Cradle.nbs", true);
+            saveResource("/songs/Dynamite.nbs", true);
+            saveResource("/songs/GangnamStyle.nbs", true);
+            saveResource("/songs/Payphone.nbs", true);
+            saveResource("/songs/Problem.nbs", true);
+        }
 
-        Song s = NBSDecoder.parse(new File(CatsCraftMain.getPlugin().getDataFolder(), "songs/Problem.nbs"));
-        sp = new RadioSongPlayer(s);
-        sp.addPlayer(p);
-        sp.setPlaying(true);
+        ArrayList<Song> songs = new ArrayList<>();
+        for (File file : files) {
+            if (!file.getName().endsWith("nbs")) continue;
+            songs.add(NBSDecoder.parse(file));
+        }
+
+        return new ObjectPager<>(size, songs);
     }
 }
