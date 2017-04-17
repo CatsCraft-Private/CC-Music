10805650e8b0b4ef3226d2387ad480dddd797a39
brainsynder-Dev <brainsnyder498@gmail.com>
2017-04-16 23:24:40 -0400

    Modified the Main class

--- src/bobcatsss/music/Main.java
+++ src/bobcatsss/music/Main.java
@@ -8,26 +8,12 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-	
-	public static Main plugin;
-	
-	
-	public HashMap<Player, Sound> pmusic = new HashMap<Player, Sound>();
+	static Main plugin;
+    HashMap<Player, Sound> pmusic = new HashMap<>();
 	
 	public void onEnable() {
-		
 		plugin = this;
-		
 		Bukkit.getPluginManager().registerEvents(new GUIClickEvent(), this);
-		
 		getCommand("music").setExecutor(new MusicGUICommand());
-		
 	}
-	
-	public void onDisable() {
-		
-		plugin = null;
-		
-	}
-
 }
