542a24e0db29860177477321e0a108c18dbc080b
CatsCraft-Private <cbbdevs@gmailcom>
2017-04-17 10:23:02 -0400

    Updated Code

--- src/bobcatsss/music/InvHolder.java
+++ src/bobcatsss/music/InvHolder.java
@@ -2,7 +2,7 @@ package bobcatsss.music;
 
 import org.bukkit.inventory.*;
 
-public class InvHolder extends InventoryHolder {
+public class InvHolder implements InventoryHolder {
     @Override public Inventory getInventory() {
         return null;
     }
