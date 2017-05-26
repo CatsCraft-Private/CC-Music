404799857fa87df49caac98693a3c609bec90e0c
CatsCraft-Private <cbbdevs@gmail.com>
2017-05-26 11:13:24 -0400

    Added 1.12 Sound Support

--- src/modded/NoteBlockAPI/Instrument.java
+++ src/modded/NoteBlockAPI/Instrument.java
@@ -2,31 +2,62 @@ package modded.NoteBlockAPI;
 
 import org.bukkit.Sound;
 import simple.brainsynder.sound.SoundMaker;
+import simple.brainsynder.utils.ServerVersion;
 
 public class Instrument {
     public Instrument() {
     }
-
+    
     public static Sound getInstrument(byte instrument) {
-        String soundName;
+        SoundMaker soundName;
         switch (instrument) {
             case 0:
-                soundName = SoundMaker.BLOCK_NOTE_HARP.getSound();//Sound.NOTE_PIANO
+                soundName = SoundMaker.BLOCK_NOTE_HARP;//Sound.NOTE_PIANO
+                break;
             case 1:
-                soundName = SoundMaker.BLOCK_NOTE_BASS.getSound();//Sound.NOTE_BASS_GUITAR;
+                soundName = SoundMaker.BLOCK_NOTE_BASS;//Sound.NOTE_BASS_GUITAR;
+                break;
             case 2:
-                soundName = SoundMaker.BLOCK_NOTE_BASEDRUM.getSound();//Sound.NOTE_BASS_DRUM;
+                soundName = SoundMaker.BLOCK_NOTE_BASEDRUM;//Sound.NOTE_BASS_DRUM;
+                break; 
             case 3:
-                soundName = SoundMaker.BLOCK_NOTE_SNARE.getSound();//Sound.NOTE_SNARE_DRUM;
+                soundName = SoundMaker.BLOCK_NOTE_SNARE;//Sound.NOTE_SNARE_DRUM;
+                break;
             case 4:
-                soundName = SoundMaker.BLOCK_NOTE_HAT.getSound();//Sound.NOTE_STICKS;
+                soundName = SoundMaker.BLOCK_NOTE_HAT;//Sound.NOTE_STICKS;
+                break;
+            
             default:
-                soundName = SoundMaker.BLOCK_NOTE_HARP.getSound();//Sound.NOTE_PIANO;
+                if (ServerVersion.getVersion().getIntVersion() >= 112) {
+                    switch (instrument) {
+                        case 5:
+                            soundName = SoundMaker.BLOCK_NOTE_FLUTE;
+                            break;
+                        case 6:
+                            soundName = SoundMaker.BLOCK_NOTE_BELL;
+                            break;
+                        case 7:
+                            soundName = SoundMaker.BLOCK_NOTE_GUITAR;
+                            break;
+                        case 8:
+                            soundName = SoundMaker.BLOCK_NOTE_CHIME;
+                            break;
+                        case 9:
+                            soundName = SoundMaker.BLOCK_NOTE_XYLOPHONE;
+                            break;
+                        default:
+                            soundName = SoundMaker.BLOCK_NOTE_HARP;//Sound.NOTE_PIANO;
+                            break;
+                    }
+                } else {
+                    soundName = SoundMaker.BLOCK_NOTE_HARP;//Sound.NOTE_PIANO;
+                }
+                break;
         }
-
-        return Sound.valueOf(soundName);
+        
+        return Sound.valueOf(soundName.getSound());
     }
-
+    
     public static org.bukkit.Instrument getBukkitInstrument(byte instrument) {
         switch (instrument) {
             case 0:
